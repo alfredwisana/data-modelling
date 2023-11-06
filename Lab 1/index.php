17d7793971116188178dfe9444e933bb69e406d4
Alfred Wisana <117831014+alfredwisana@users.noreply.github.com>
2023-11-06 21:19:38 +0700

    lab 1

--- Lab 1/index.php
+++ Lab 1/index.php
@@ -3,27 +3,38 @@
 
 <head>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,900,900i" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,900,900i" rel="stylesheet">
 
-        <title>Redis</title>
-        <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <title>Redis</title>
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 
-    </head>
+
 
     <style>
         .table {
             margin: 0 auto;
             position: relative;
-            width: 50%;
+            width: 25%;
+            margin-top: 2rem;
+            text-align: center;
+            border: 1px solid black;
+        }
+
+        h1 {
             margin-top: 5rem;
-            text-align: center
+
+            text-align: center;
+            font-weight: bold;
+        }
+
+        input {
+            width: 50%;
         }
 
         ;
@@ -35,10 +46,9 @@
 
     <table class="table">
         <thead>
-            <tr>
-                <th scope="col">PEOPLE</th>
-            </tr>
+            <h1>PEOPLE</h1>
         </thead>
+
         <tbody>
             <?php
             require 'Predis/Predis/Autoload.php';
@@ -50,16 +60,14 @@
                 'host' => '127.0.0.1',
                 'port'  => 6379
             ]);
-            $i = 1;
+
             $peopleName = $redis->lrange('people', 0, -1);
-            foreach ($peopleName as $name){
+            foreach ($peopleName as $name) {
                 echo "<tr>
-                <td>$i</td>
                 <td>$name</td>
                 </tr>";
-                $i = $i+1;
             };
-            
+
 
             ?>
 
@@ -76,13 +84,13 @@
             </tr>
         </tbody>
     </table>
-    
+
 </body>
 
 
 <script>
     $(document).ready(function() {
-        
+
 
         function callFunction(functionName) {
             v_nama = $("#nama").val();
@@ -94,7 +102,7 @@
                     nama: v_nama
                 },
                 success: function(result) {
-                    // alert(result);
+                    alert(result);
                     window.location.reload();
                 }
             });
@@ -127,24 +135,21 @@
 function lpush($nama)
 {
     global $redis;
-    $len = $redis -> llen('people');
-    if ($len < 10){
+    $len = $redis->llen('people');
+    if ($len < 10) {
         $redis->lpush('people', $nama);
-    }
-    else{
+    } else {
         echo 'List already has 10 data';
     }
-    
 }
 
 function rpush($nama)
 {
     global $redis;
-    $len = $redis -> llen('people');
-    if ($len < 10){
+    $len = $redis->llen('people');
+    if ($len < 10) {
         $redis->rpush('people', $nama);
-    }
-    else{
+    } else {
         echo 'List already has 10 data';
     }
 }
@@ -165,13 +170,21 @@ if (isset($_GET['action'])) {
     $action = $_GET['action'];
     if (function_exists($action)) {
         if ($action === 'lpush') {
-            $value = $_GET['nama'];
-            lpush($value);
+            if (isset($_GET['nama'])) {
+                $value = $_GET['nama'];
+                if (!empty($value)) {
+                    lpush($value);
+                }
+            }
         } elseif ($action === 'lpop') {
             lpop();
         } elseif ($action === 'rpush') {
-            $value = $_GET['nama'];
-            rpush($value);
+            if (isset($_GET['nama'])) {
+                $value = $_GET['nama'];
+                if (!empty($value)) {
+                    rpush($value);
+                }
+            }
         } elseif ($action  === 'rpop') {
             rpop();
         }
