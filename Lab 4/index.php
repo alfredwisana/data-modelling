a3b5724a84e65353acbb5f09f936ad00ad127508
Alfred Wisana <117831014+alfredwisana@users.noreply.github.com>
2023-12-18 09:11:11 +0700

    neo4j

---
+++ Lab 4/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'autoload.php';
+
+$client = Laudis\Neo4j\ClientBuilder::create()->withDriver('default', 'bolt://neo4j:c14210177@localhost')->build();
+
+
+$result = $client->run(<<<'CYPHER'
+MATCH (people:Person) RETURN people
+CYPHER, ['dbName' => 'neo4j']);
+
+
+
+
+?>
